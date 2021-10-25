22704ba61e56e4b4d9fdf49f4ad8892268297aa1
yamashiro0110 <yamashiro0110@gmail.com>
2021-10-25 16:40:39 +0900

    Update welcome_message.php
    add Link

--- webapp/app/Views/welcome_message.php
+++ webapp/app/Views/welcome_message.php
@@ -208,6 +208,7 @@
 				<button onclick="toggleMenu();">&#9776;</button>
 			</li>
 			<li class="menu-item hidden"><a href="/account">Account</a></li>
+			<li class="menu-item hidden"><a href="/form">Form</a></li>
 			<li class="menu-item hidden"><a href="#">Home</a></li>
 			<li class="menu-item hidden"><a href="https://codeigniter4.github.io/userguide/" target="_blank">Docs</a>
 			</li>
